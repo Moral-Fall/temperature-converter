c8f9538dd16e10166457280c55d3358352ff6b1f
Domi <guests@mac.local>
2025-12-13 12:08:15 -0700

    moving on to propmt. error seem to have ran into error where prmpt num is not showing up

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-function converToCelsius (farhrenheit){
-    const celsius = (farhrenheit - 32) * 5 / 9;
+function convertToCelsius(fahrenheit){
+    const celsius = (fahrenheit - 32) * 5 / 9;
     return celsius;
 }
-console.log(converToCelsius)
+console.log(convertToCelsius(100));
+
+function describeTemperature(temp){
+    if (temp < 0) { 
+        return "Very Cold"; 
+}
+    else if (temp < 20) {return "cold";
+
+    }
+    else if (temp < 30) {
+        return "warm";
+
+    }
+    else if (temp < 40) {
+        return "hot";
+
+    }
+    else {
+        return "very hot";
+
+    }
+}
+console.log(describeTemperature(25));
+
+
+const fahrenhiet = number(prompt ("Give me a temperature"))
+
+const celsius = convertToCelsius(fahrenhiet)
+const description = describeTemperature(fahrenhiet)
+
